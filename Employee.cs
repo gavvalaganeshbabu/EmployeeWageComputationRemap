5813ad8a502cb7e3de56fb31a73a60dedcb6394f
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-04-01 14:03:28 +0530

    Switch Case Statements Usage Code Added

--- Employee.cs
+++ Employee.cs
@@ -9,14 +9,29 @@ namespace EmployeeWageProblem
         const int WagePerHour = 20;
         const int TotalHourPerDay = 8;
         const int PartTimeHour = 4;
-        public  string IsEmployeePresent() {
+        public  void IsEmployeePresent() {
+            Console.WriteLine("Enter Employee Name :");
+            string name = Console.ReadLine();
             Random rnd = new Random();
             int attendence = rnd.Next(0, 3);
-            if (attendence == 0)
-                return "Absent ";
-            else if (attendence == 1) return "Part Time Present";
-            else
-                return "Full Day Present";
+            switch (attendence)
+            {
+                case 0:
+                    Console.WriteLine(name+"Absent Today and No Income Today :");
+                    break;
+                case 1:
+                    Console.WriteLine(name + " Is Present Half Today ");
+                    Console.WriteLine(PartTimeWageCalculator()+" Is Income Of Today :");
+                    break;
+                case 2:
+                    Console.WriteLine(name + " Is Present Full day ");
+                    Console.WriteLine(WagesPerDay() + " Is Income Today ");
+                    break;
+                default:
+                    Console.WriteLine("invalid Option");
+                    break;
+
+            }
         }
         public int WagesPerDay() {
             int WageperDay = WagePerHour * TotalHourPerDay;
--- Program.cs
+++ Program.cs
@@ -7,23 +7,9 @@ namespace EmployeeWageProblem
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Project : ");
-            Console.WriteLine("Enter Employee Name :");
-            string name=Console.ReadLine();
+            
             Employee employee= new Employee();
-            string b = employee.IsEmployeePresent();
-            if (b == "Full Day Present") {
-                Console.WriteLine(name + " Is Present Today ");
-                int WagePerDay=employee.WagesPerDay();
-                Console.WriteLine(WagePerDay+" Is the Total Wages Per Day ");
-            }
-            else if(b== "Part Time Present")
-            {
-                Console.WriteLine(name + " Is Present Half Today ");
-                int WagePerHalfDay = employee.PartTimeWageCalculator();
-                Console.WriteLine(WagePerHalfDay + " Is Total Wage Today ");
-            }
-            else { Console.WriteLine(name + "Is Absent Today And No Income Today ");
-            }
+            employee.IsEmployeePresent();
             Console.ReadLine();
         }
 
